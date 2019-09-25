cdcfd51b494dc3048e349b3bf273a3f28c7f4704
Felipe Mendes <felipe.m.alex@hotmail.com>
2019-09-25 15:45:30 -0300

    Fase 24: Implementando a busca Simples

--- SalesWeb-MVC/Controllers/RegistroVendaController.cs
+++ SalesWeb-MVC/Controllers/RegistroVendaController.cs
@@ -2,20 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWeb_MVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SalesWeb_MVC.Controllers
 {
     public class RegistroVendaController : Controller
     {
+        private readonly RegistroVendaService _registroVenda;
+
+        public RegistroVendaController(RegistroVendaService registroVenda)
+        {
+            _registroVenda = registroVenda;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult BuscaSimples()
+        public async Task<IActionResult> BuscaSimples(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if(!minDate.HasValue)
+            {
+                minDate = new DateTime(2018,1,1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _registroVenda.FindbyDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult BuscaGrupo()
---
+++ SalesWeb-MVC/Services/RegistroVendaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWeb_MVC.Data;
+using SalesWeb_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SalesWeb_MVC.Services
+{
+    public class RegistroVendaService
+    {
+        private readonly SalesWeb_MVCContext _MVCContext;
+
+        public RegistroVendaService(SalesWeb_MVCContext context)
+        {
+            _MVCContext = context;
+        }
+
+        public async Task<List<RegistroVenda>> FindbyDateAsync(DateTime? minDate, DateTime? maxDate) // busca o registro por data
+        {
+            var result = from obj in _MVCContext.RegistroVenda select obj;  
+            if(minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if(maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x=>x.Vendedor)
+                .Include(x=>x.Vendedor.Departamento)
+                .OrderByDescending(x=>x.Data)
+                .ToListAsync();
+        }
+    }
+}
